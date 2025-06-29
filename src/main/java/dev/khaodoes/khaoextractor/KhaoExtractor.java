ad61c6e8bb69f5cbf25c9e1139f836920f3ac758
Khao <khao@khaodoes.dev>
2025-06-29 17:22:48 +0700

    Init

---
+++ src/main/java/dev/khaodoes/khaoextractor/KhaoExtractor.java
@@ -0,0 +1,10 @@
+package dev.khaodoes.khaoextractor;
+
+import net.fabricmc.api.ModInitializer;
+
+public class KhaoExtractor implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
